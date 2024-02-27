11aa72eddca7206335f73e3c7f54c4b86f12404d
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-02-28 12:04:04 +1300

    Simple example done

--- generate.py
+++ generate.py
@@ -5,6 +5,7 @@ Generate a simple example of the quasar time dilation data.
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as rng
+import pandas as pd
 
 # Set random seed
 rng.seed(123)
@@ -12,13 +13,30 @@ rng.seed(123)
 # Number of quasars
 N = 1000
 
+# Cosmology parameter
+n = 1.0
+
 # Generate some redshifts from a lognormal distribution
 z = np.exp(0.3*rng.randn(N))
 
 # Generate some 'true' variability timescales (in 'days')
-log10_tau = 2.0 + 0.3*rng.randn(N)
+log10_tau_restframe = 2.0 + 0.1*rng.randn(N)
+
+# Apply the redshift
+log10_tau_now = log10_tau_restframe + n*np.log10(1.0 + z)
+
+# Measurement noise
+log10_tau_measured = log10_tau_now + 0.1*rng.randn(N)
+
+# Save as CSV
+data = pd.DataFrame()
+data["z"] = z
+data["log10_tau_lower"] = log10_tau_measured - 0.1
+data["log10_tau_mid"] = log10_tau_measured
+data["log10_tau_upper"] = log10_tau_measured + 0.1
+data.to_csv("data.csv", index=False)
+
+plt.errorbar(z, log10_tau_measured, fmt="o", yerr=0.1,  alpha=0.3)
+plt.show
 
 
-
-plt.hist(10.0**log10_tau, 100)
-plt.show()
