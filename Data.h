3ff58b4eaabed2b65c6d4ac61bc7fba8ff630d0c
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-11-20 13:54:07 +1300

    Fixed mistake

--- Data.h
+++ Data.h
@@ -14,11 +14,14 @@ class Data
         std::vector<double> log10_tau_lower;
         std::vector<double> log10_tau_mid;
         std::vector<double> log10_tau_upper;
+        std::vector<double> m_bh;
+        std::vector<double> m_bh_err;
 
         // Summaries of explanatory variables
         double mean_zz; // Where zz = log10(1+z)
         double mean_lambda;
         double mean_l_bol;
+        double mean_m_bh;
 
     public:
         Data(const char* filename);
